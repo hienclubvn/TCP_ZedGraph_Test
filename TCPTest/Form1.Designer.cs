f9584e66597e9aa11b0a4e87d7d17fcb7ed54287
hienclubvn <danghien719@gmail.com>
2020-07-17 11:19:23 +0700

    Update All

--- TCPTest/Form1.Designer.cs
+++ TCPTest/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnConnect = new System.Windows.Forms.Button();
             this.txbAddress = new System.Windows.Forms.TextBox();
             this.nmPort = new System.Windows.Forms.NumericUpDown();
@@ -35,12 +36,15 @@
             this.btnSend = new System.Windows.Forms.Button();
             this.txbSend = new System.Windows.Forms.TextBox();
             this.btnDisconnect = new System.Windows.Forms.Button();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.nmPort)).BeginInit();
             this.SuspendLayout();
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(476, 11);
+            this.btnConnect.Location = new System.Drawing.Point(296, 9);
             this.btnConnect.Name = "btnConnect";
             this.btnConnect.Size = new System.Drawing.Size(75, 23);
             this.btnConnect.TabIndex = 0;
@@ -51,23 +55,23 @@
             // 
             this.txbAddress.Location = new System.Drawing.Point(12, 12);
             this.txbAddress.Name = "txbAddress";
-            this.txbAddress.Size = new System.Drawing.Size(332, 20);
+            this.txbAddress.Size = new System.Drawing.Size(161, 20);
             this.txbAddress.TabIndex = 1;
-            this.txbAddress.Text = "192.168.1.8";
+            this.txbAddress.Text = "192.168.4.1";
             // 
             // nmPort
             // 
-            this.nmPort.Location = new System.Drawing.Point(350, 13);
+            this.nmPort.Location = new System.Drawing.Point(190, 11);
             this.nmPort.Maximum = new decimal(new int[] {
             65000,
             0,
             0,
             0});
             this.nmPort.Name = "nmPort";
-            this.nmPort.Size = new System.Drawing.Size(120, 20);
+            this.nmPort.Size = new System.Drawing.Size(82, 20);
             this.nmPort.TabIndex = 2;
             this.nmPort.Value = new decimal(new int[] {
-            9000,
+            80,
             0,
             0,
             0});
@@ -77,12 +81,13 @@
             this.txbData.Location = new System.Drawing.Point(12, 39);
             this.txbData.Multiline = true;
             this.txbData.Name = "txbData";
-            this.txbData.Size = new System.Drawing.Size(458, 253);
+            this.txbData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txbData.Size = new System.Drawing.Size(458, 433);
             this.txbData.TabIndex = 3;
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(477, 302);
+            this.btnSend.Location = new System.Drawing.Point(397, 487);
             this.btnSend.Name = "btnSend";
             this.btnSend.Size = new System.Drawing.Size(75, 23);
             this.btnSend.TabIndex = 0;
@@ -91,26 +96,57 @@
             // 
             // txbSend
             // 
-            this.txbSend.Location = new System.Drawing.Point(12, 305);
+            this.txbSend.Location = new System.Drawing.Point(12, 488);
             this.txbSend.Name = "txbSend";
-            this.txbSend.Size = new System.Drawing.Size(458, 20);
+            this.txbSend.Size = new System.Drawing.Size(379, 20);
             this.txbSend.TabIndex = 1;
+            this.txbSend.Text = "$$,11.1444,6.2323,13.4445,$$       ";
             // 
             // btnDisconnect
             // 
-            this.btnDisconnect.Location = new System.Drawing.Point(476, 40);
+            this.btnDisconnect.Location = new System.Drawing.Point(395, 10);
             this.btnDisconnect.Name = "btnDisconnect";
             this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
             this.btnDisconnect.TabIndex = 4;
             this.btnDisconnect.Text = "Disconnect";
             this.btnDisconnect.UseVisualStyleBackColor = true;
-            this.btnDisconnect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(476, 11);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(656, 497);
+            this.zedGraphControl1.TabIndex = 5;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(481, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 400;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(564, 334);
+            this.ClientSize = new System.Drawing.Size(1142, 522);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.zedGraphControl1);
             this.Controls.Add(this.btnDisconnect);
             this.Controls.Add(this.txbData);
             this.Controls.Add(this.nmPort);
@@ -119,7 +155,7 @@
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.btnConnect);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Check Voltage for System Nitfy";
             ((System.ComponentModel.ISupportInitialize)(this.nmPort)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -135,6 +171,10 @@
         private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.TextBox txbSend;
         private System.Windows.Forms.Button btnDisconnect;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Timer timer1;
+        //private ZedGraphView.ZedGraphControl zedGraphControl1;
     }
 }
 
--- TCPTest/Form1.cs
+++ TCPTest/Form1.cs
@@ -8,16 +8,55 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using SocketIP;
+//using ZedGraphView;
 
 namespace TCPTest
 {
     public partial class Form1 : Form
     {
         CClientSocket soc;
+        ZedGraphView zedview;
+        double volt, volt1, volt2;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Invoke((MethodInvoker)delegate
+                    {
+                        //Format receive: $$,11.1444,6.2323,13.4445,$$  (Hercules - TCP Server)
+                        //string strdata = soc.ReceivedText;
+                        byte [] bytes = soc.ReceivedBytes;
+                        string strdata = Encoding.Default.GetString(bytes);
+                        //txbData.AppendText(string.Format("Receive {0}:: Data: {1}", _soc.ToString(), strdata));
+                        txbData.AppendText(strdata + "  \r\n");
+                        string[] subdata = strdata.Split(',');
+                        int index = 0;
+                        for (index = 0; index < subdata.Length; index++)
+                        {
+                            if (subdata[index] == "#")
+                            {
+                                break;
+                            }
+                        }
+                    //
+                    if ((index > 0) && (subdata.Length > index + 3))
+                        {
+                            double.TryParse(subdata[index+1], out volt); //Volt 1
+                            double.TryParse(subdata[index+2], out volt1); //Volt 2
+                            double.TryParse(subdata[index+3], out volt2); //Volt 3
+                            zedview.Draw(volt, volt1, volt2);
+                        }
+                    });
+               } 
+
+
         public Form1()
         {
             InitializeComponent();
 
+            zedview = new ZedGraphView(ref zedGraphControl1); //init
+            zedview.Init();
+
+            //Events ----
             btnConnect.Click += (o, e) =>
             {
                 txbData.Text = string.Format("Connect to: {0}:{1}", txbAddress.Text, nmPort.Value);
@@ -28,15 +67,37 @@ namespace TCPTest
                     {
                         txbData.AppendText(string.Format("Socket {0} is connected", _soc.RemoteEndPoint.ToString()));
                         //btnConnect.Text = "Disconnect";
+                        timer1.Enabled = true;
                     });
+                    
                 };
-                soc.OnRead += (_soc) =>
-                {
-                    Invoke((MethodInvoker)delegate
-                    {
-                        txbData.AppendText(string.Format("Receive {0}:: Data: {1}", _soc.ToString(), soc.ReceivedText));
-                    });
-                };
+                //soc.OnRead += (_soc) =>
+                //{
+                //    Invoke((MethodInvoker)delegate
+                //    {
+                //        //Format receive: $$,11.1444,6.2323,13.4445,$$  (Hercules - TCP Server)
+                //        string strdata = soc.ReceivedText;
+                //        //txbData.AppendText(string.Format("Receive {0}:: Data: {1}", _soc.ToString(), strdata));
+                //        txbData.AppendText(strdata + "  \r\n");
+                //        string[] subdata = strdata.Split(',');
+                //        //
+                //        if (subdata[0] == "$")
+                //        {
+                //            if (subdata.Length >= 3)
+                //            {
+                //                if (subdata[1] != null)
+                //                {
+                //                    double.TryParse(subdata[1], out volt); //Volt 1
+                //                    double.TryParse(subdata[2], out volt1); //Volt 2
+                //                    double.TryParse(subdata[3], out volt2); //Volt 3
+                //                    zedview.Draw(volt, volt1, volt2);
+                //                }
+                //            }
+
+                            
+                //        }
+                //    });
+                //};
                 soc.OnDisconnect += (_soc) =>
                 {
                     Invoke((MethodInvoker)delegate
@@ -48,15 +109,17 @@ namespace TCPTest
                 soc.Connect();
             };
 
+            btnDisconnect.Click += (o, e) =>
+            {
+                soc.Disconnect();
+            };
+            //
             btnSend.Click += (o, e) =>
             {
                 soc.SendText(txbSend.Text);
             };
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            soc.Disconnect();
+            //
+            btnClear.Click += (o, e) => zedview.Init();
         }
     }
 }
